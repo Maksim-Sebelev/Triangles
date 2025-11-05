fc490a1ad88f7701ff706aab3bb2cc91f8f910a2
Maksim-Sebelev <7905111@list.ru>
2025-11-06 02:44:37 +0300

    ZoV Update python script to parse tests results: 	add percente of accuracy (quantity of correct find triangles / quantity of all triangles)

--- Src/debug/parse_program_output/parse_result.py
+++ Src/debug/parse_program_output/parse_result.py
@@ -20,25 +20,32 @@ def color_print(color, arg, **kwargs):
 
 def main():
     if len(sys.argv) != 4:
-        print(f"Usage: python3 {sys.argv[0]}.py <run_traingles_exe> <test.dat> <test.ans>")
+        print(f"Usage: {sys.argv[0]}.py <run_traingles_exe> <test.dat> <test.ans>")
         sys.exit(1)
-    
+
     executable, test_dat, ans_dat = sys.argv[1:4]
+    n_triangles = 0
 
     try:
         with open(test_dat, 'r') as f:
+            first_line = f.readline().strip()
+            n_triangles = int(first_line)
+
+            f.seek(0)
+
             result = subprocess.run(
                 [executable],
                 stdin=f,
                 capture_output=True,
                 text=True
             )
-        
+
         if result.returncode != 0:
-            print(f"Error: Program failed with exit code {result.returncode}")
-            print(f"Stderr: {result.stderr}")
+            color_print(Colors.RED, f"Error: Program failed with exit code {result.returncode}")
+            print(f"stderr: {result.stderr}")
+            print(f"stdin: ${result.stdout}")
             sys.exit(1)
-        
+
         program_good_triangles = []
         for line in result.stdout.strip().split():
             try:
@@ -47,9 +54,9 @@ def main():
                     program_good_triangles.append(num)
             except ValueError:
                 continue
-                
+
     except Exception as e:
-        print(f"Error running program: {e}")
+        color_print(Colors.RED, f"Error running program: {e}")
         sys.exit(1)
 
     try:
@@ -64,7 +71,7 @@ def main():
                     correct_good_triangles.append(num)
             except ValueError:
                 continue
-                
+
     except Exception as e:
         print(f"Error reading answer file: {e}")
         sys.exit(1)
@@ -73,25 +80,50 @@ def main():
         color_print(Colors.GREEN, "TEST PASSED", end = '\n')
         return 0
 
-    color_print(Colors.WHITE, "Find triangles:")
+    errors_quantity = 0
 
-    for triangles_num in program_good_triangles:
-        if triangles_num in correct_good_triangles:
-            color_print(Colors.GREEN, triangles_num, end = ' ')
+    color_print(Colors.WHITE, "Found triangles:")
+
+    if len(program_good_triangles) == 0:
+        if len(correct_good_triangles) == 0:
+            color_print(Colors.GREEN, "No triagles was found")
         else:
-            color_print(Colors.RED, triangles_num, end = ' ')
+            color_print(Colors.RED, "No triagles was found")
+    else:
+        for triangles_num in program_good_triangles:
+            if triangles_num in correct_good_triangles:
+                color_print(Colors.GREEN, triangles_num, end = ' ')
+            else:
+                color_print(Colors.RED, triangles_num, end = ' ')
+                errors_quantity += 1
 
     print('\n')
-    color_print(Colors.WHITE, "Not find triangles:")
+    color_print(Colors.WHITE, "Not found triangles:")
 
-    for triangles_num in correct_good_triangles:
+
+    for triangles_num in range(0, n_triangles):
         if triangles_num in program_good_triangles:
-            color_print(Colors.GREEN, triangles_num, end = ' ')
-        else:
-            color_print(Colors.RED, triangles_num, end = ' ')
+            continue
 
-    print('\n')
-    color_print(Colors.RED, "TEST FAILED", end = '\n')
+        if triangles_num in correct_good_triangles:
+            color_print(Colors.RED, triangles_num, end = ' ')
+            errors_quantity += 1
+
+        else:
+            color_print(Colors.GREEN, triangles_num, end = ' ')
+
+    color_print(Colors.WHITE, "\n\nINFO:", end = '\n\n')
+
+    color_print(Colors.WHITE, f"[found triangles]: {len(program_good_triangles)}/{len(correct_good_triangles)}", end = "\n\n")
+
+    percent_of_errors = (errors_quantity / n_triangles) * 100
+    percent_of_accuracy = 100 - percent_of_errors 
+
+    color_print(Colors.GREEN, f"[percentage of accuracy]: {round(percent_of_accuracy, 2)}%")
+    color_print(Colors.RED  , f"[percentage of errors  ]: {round(percent_of_errors  , 2)}%")
+
+    color_print(Colors.RED, "\n\nTEST FAILED", end = "\n")
+
     return 1
 
 if __name__ == "__main__":
